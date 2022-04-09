710a47be3a1c421ab7e3f25706a66bc06a20bc01
akinari <a.mizo0919@gmail.com>
2022-04-09 19:13:20 +0900

    add:i18nの設定 #12

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,12 @@ module MajiInfo
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    # 言語・タイムゾーンを日本に設定
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
