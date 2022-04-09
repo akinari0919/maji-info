919604b0ea3f35d1ed4e390a5f52d2e8bfe5fd8d
akinari <a.mizo0919@gmail.com>
2022-04-09 18:39:57 +0900

    add:genarateコマンドでの生成ファイル制限 #11

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,11 @@ module MajiInfo
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.generators do |g|
+      g.assets false
+      g.skip_routes true
+      g.helper false
+    end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
