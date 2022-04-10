1b42b078e9451e1581a11689277c1c78e53e4cdd
akinari <a.mizo0919@gmail.com>
2022-04-11 00:30:40 +0900

    add:ログイン遷移実装 #9

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,4 +1,21 @@
 class UserSessionsController < ApplicationController
-  def new
+  skip_before_action :require_login, only: %i[new create]
+
+  def new; end
+
+  def create
+    @user = login(params[:email], params[:password])
+
+    if @user
+      redirect_back_or_to root_path, notice: t(".success")
+    else
+      flash.now[:alert] = t(".failure")
+      render :new
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to root_path, notice: t(".success")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  root 'user_sessions#new'
+  root "user_sessions#new"
+  post "login", to: "user_sessions#create"
+  delete "logout", to: "user_sessions#destroy"
 end
