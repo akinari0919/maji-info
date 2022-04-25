ecf7bf3dd40accd83d34d1ba6d9bc6a96bc2ae78
Ryosuke <tffcd0514@gmail.com>
2022-04-25 15:05:22 +0900

    add: 追加したカラムにバリデーション追加 #34

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ApplicationRecord
   validates :url, presence: true, uniqueness: true
+  validates :body, presence: true, uniqueness: true
+  validates :title, presence: true, uniqueness: true
+  validates :contributor, presence: true
 
   scope :random, -> { offset(rand(Post.count)).first }
 end
