686227fd70b41e5c998c7779aa42488d35731b79
Ryosuke <tffcd0514@gmail.com>
2022-04-25 16:01:22 +0900

    add: カラムの追加に合わせてコントローラとviewを修正 #34

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -44,6 +44,6 @@ class PostsController < ApplicationController
     end
 
     def post_params
-      params.require(:post).permit(:body)
+      params.require(:post).permit(:title, :body, :url, :contributor)
     end
 end
